693b04f282ba7855268d4fc8037db9d82d1c4799
Avijit Nanda <avijitnanda14@gmail.com>
2018-09-01 12:11:05 +0530

    added rest files

---
+++ js/floating.js
@@ -0,0 +1,10 @@
+// When the user scrolls down 20px from the top of the document, show the button
+
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
